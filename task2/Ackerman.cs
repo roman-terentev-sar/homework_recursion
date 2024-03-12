88b5ff1beff261ad15e7c1163ca854a140e1fa6f
Terentev Roman <saradventure@andex.ru>
2024-03-12 20:43:20 +0400

    first commit

---
+++ task2/Ackerman.cs
@@ -0,0 +1,15 @@
+﻿int firstNum = new Random().Next(1, 4);
+Console.WriteLine("m = " + firstNum);
+
+int secondNum = new Random().Next(1, 6);
+Console.WriteLine("n = " + secondNum);
+
+int Ackerman(int m, int n)
+{
+    if(m == 0) return n+1;
+    else if(n == 0) return Ackerman(m - 1, 1);
+    else return Ackerman(m-1, Ackerman(m, n-1));
+
+}
+
+Console.WriteLine(Ackerman(firstNum, secondNum));
