f0ecf7cb3e94d4035be92e29afc2cdf7b1bd900c
Terentev Roman <saradventure@andex.ru>
2024-03-13 10:31:46 +0400

    second try

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int m = new Random().Next(1,10);
+int n = new Random().Next(20, 100);
+
+void PrintNum(int newn)
+{
+    Console.WriteLine(newn);
+    if(newn ==m) return;
+    PrintNum(newn-1);
+}
+
+PrintNum(n);
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = new Random().Next(2, 10);
+int[] array = new int[num];
+
+for(int i = array.Length-1; i>= 0; i--)
+{
+    array[i] = new Random().Next(1, 99);
+    Console.WriteLine(array[i]);
+}
+
+Console.WriteLine();
+
+void PrintArray(int[] array2, int i)
+{
+    if(i == 0) return;
+    PrintArray(array2, i-1);
+    Console.WriteLine(array2[i-1]);
+}
+
+PrintArray(array, num);
